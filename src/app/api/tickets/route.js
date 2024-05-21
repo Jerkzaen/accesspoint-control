f8a8e2bffce689ec5197db7a1464b68273756d8f
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-21 19:16:44 -0400

    feat: Connect to database in tickets API route
    This commit adds a database connection in the tickets API route. The `connectDB` function is called to establish a connection to the database before returning the response. This change ensures that the API route can interact with the database and retrieve the necessary data.

--- src/app/api/tickets/route.js
+++ src/app/api/tickets/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "@/utils/mongoose";
 
 //CRUD 
 
 // OBTENER TODOS LOS TICKETS
 export function GET() {
+  connectDB();
   return NextResponse.json({
     message: "Obteniendo tickets",
   });
---
+++ src/models/Ticket.js
@@ -0,0 +1,21 @@
+// Importar modulos de mongoose para crear el modelo de Ticket
+import { Schema, model, models } from "mongoose";
+
+// Crear el esquema de Ticket
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Title is required"],
+    trim: true,
+  },
+  timeStamps: true,
+});
+
+// Exportar el modelo de Ticket
+export default models.Ticket || model("Ticket", taskSchema);
