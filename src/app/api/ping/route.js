263f60595283d0f579d03284b6fab7d58db7cd6d
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-21 19:01:36 -0400

    feat: Connect to database in ping API route
    This commit adds a database connection in the ping API route. The `connectDB` function is called to establish a connection to the database before returning the response. This change ensures that the API route can interact with the database and retrieve the necessary data.

--- src/app/api/ping/route.js
+++ src/app/api/ping/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "@/utils/mongoose";
 
 export function GET() {
+  connectDB();
   return NextResponse.json({
     message: "Hello World!",
   });
