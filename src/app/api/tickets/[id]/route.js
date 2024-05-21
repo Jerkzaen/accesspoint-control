1b5304e2e979603e3cb44155266bd0c1a87e1c50
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-21 18:08:25 -0400

    feat: Add CRUD API routes for tickets
    This commit adds CRUD API routes for tickets. It includes a GET route to retrieve a specific ticket, a DELETE route to remove a ticket, and a PUT route to update a ticket. These routes will allow users to perform necessary operations on tickets in the ticket system.

--- src/app/api/tickets/[id]/route.js
+++ src/app/api/tickets/[id]/route.js
@@ -1,8 +1,24 @@
 import { NextResponse } from "next/server";
 
+//CRUD 
+
 // OBTENER UN TICKET
-export function GET() {
-  return NextResponse.json({
-    message: "Obteniendo un ticket",
-  });
+export function GET(request, { params }) {
+return NextResponse.json({
+    message: `Obteniendo un ticket ${params.id}`,
+});
+}
+
+// ELIMINAR UN TICKET
+export function DELETE(request, { params }) {
+return NextResponse.json({
+    message: `Eliminando un ticket ${params.id}`,
+});
+}
+
+// ACTUALIZAR UN TICKET
+export function PUT(request, { params }) {
+return NextResponse.json({
+    message: `Actualizando un ticket ${params.id}`,
+});
 }
