af647133f4e8edb691a5aa67466cc53a32fd0beb
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-21 19:29:08 -0400

    feat: Corregir esquema de Ticket para incluir descripción

--- src/models/Ticket.js
+++ src/models/Ticket.js
@@ -2,24 +2,21 @@
 import { Schema, model, models } from "mongoose";
 
 // Crear el esquema de Ticket
-const taskSchema = new Schema(
-  {
+const taskSchema = new Schema({
     title: {
-      type: String,
-      required: [true, "Title is required"],
-      unique: true,
-      trim: true,
+        type: String,
+        required: [true, "Title is required"],
+        unique: true,
+        trim: true,
     },
     description: {
-      type: String,
-      required: [true, "Title is required"],
-      trim: true,
-    },
-  },
-  {
+        type: String,
+        required: [true, "Title is required"],
+        trim: true,
+        }
+    }, {
     timestamps: true,
-  }
-);
+});
 
 // Exportar el modelo de Ticket
 export default models.Ticket || model("Ticket", taskSchema);
