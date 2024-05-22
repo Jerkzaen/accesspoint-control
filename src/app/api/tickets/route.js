b08668fed784d749e3c221e5d22bfc0fc5633af9
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-21 20:04:16 -0400

    feat: Crear endpoint POST para crear un ticket
    Este commit agrega un nuevo endpoint POST en la API de tickets para permitir la creación de un ticket. Se implementa la lógica necesaria para recibir los datos del ticket en el cuerpo de la petición, crear un nuevo objeto Ticket con esos datos, guardar el ticket en la base de datos y devolver el ticket creado como respuesta en formato JSON. Este cambio permite a los usuarios crear nuevos tickets a través de la API.

--- src/app/api/tickets/route.js
+++ src/app/api/tickets/route.js
@@ -4,7 +4,7 @@ import Ticket from "@/models/Ticket";
 
 //CRUD
 
-// OBTENER TODOS LOS TICKETS
+// Obtener todos los tickets
 export async function GET() {
   // Conectar a la base de datos de MongoDB
   connectDB();
@@ -14,9 +14,23 @@ export async function GET() {
   return NextResponse.json(tickets);
 }
 
-// CREAR UN TICKET
-export function POST() {
-  return NextResponse.json({
-    message: "Creando un ticket",
-  });
+// Crear un ticket
+export async function POST(request) {
+  try {
+    // Datos del ticket a crear en el body de la petición
+    const data = await request.json();
+    // Crear un nuevo ticket con los datos del body
+    const newTask = new Ticket(data);
+    // Guardar el ticket en la base de datos
+    const saveTicket = await newTask.save();
+    // Devolver datos del ticket creado
+    console.log(saveTicket);
+    // Devolver el ticket creado en formato JSON
+    return NextResponse.json(saveTicket);
+  } catch (error) {
+    // Devolver un mensaje de error si no se pudo crear el ticket
+    return NextResponse.json(error.message, {
+      status: 400,
+    });
+  }
 }
