f78b8bdd203c059d2388491ba819cfdbc7e7a0de
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-22 23:15:57 -0400

    feat: Importar el componente TicketForm y renderizarlo en la página NewPage

--- src/app/tickets/new/page.tsx
+++ src/app/tickets/new/page.tsx
@@ -1,5 +1,7 @@
+// Importar el componente TicketForm y renderizarlo en la página NewPage.
 import { TicketForm } from "@/components/ticket-form";
 
+// Definir la función NewPage que renderiza el componente TicketForm.  
 function NewPage() {
   return (
     <div className="flex justify-center items-center h-screen">
@@ -7,4 +9,5 @@ function NewPage() {
     </div>
   );
 }
+// Exportar la función NewPage.
 export default NewPage;
