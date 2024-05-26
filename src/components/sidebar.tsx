668794bd63608ce5972279ad3b4464510886a648
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-25 20:14:55 -0400

    Actualizar componente Sidebar para agregar enlaces de navegación en la barra lateral

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,13 +2,18 @@
 //renderizar el componente Sidebar y SidebarDesktop en el navegador y no en el servidor de la aplicación de Next.js
 "use client";
 // Importamos el componente Home de lucide-react
-import { Home } from "lucide-react";
+import { Bell, Home, User } from "lucide-react";
 // importamos el componente SidebarDesktop
 import { SidebarDesktop } from "./sidebar-desktop";
 import { SidebarItem } from "@/types";
-
+// Definimos el objeto sidebarItems que contiene un arreglo de links con un objeto que contiene un href y un label
 const sidebarItems: SidebarItem = {
-  links: [{ href: "/", label: "Home", icon: Home }],
+  links: [
+    {label: "Dashboard", href: "/" , icon: Home },
+    {label: "Notificaciones", href: "/item/notificacion" , icon: Bell },
+    {label: "Perfil", href: "/item/perfil" , icon: User }
+
+],
 };
 
 // Definimos la función Sidebar que devuelve el componente SidebarDesktop
