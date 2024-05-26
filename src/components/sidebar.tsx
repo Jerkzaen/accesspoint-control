f76c3fcf4219c86f3a6756acf5e1c85fc1a85328
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-25 20:03:18 -0400

    Actualizar componente SidebarDesktop para agregar enlaces con funcionalidad de navegación

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,10 +1,12 @@
-//
+//  use cliente es una directiva que indica que se va a usar el cliente para 
+//renderizar el componente Sidebar y SidebarDesktop en el navegador y no en el servidor de la aplicación de Next.js   
 "use client";
+// Importamos el componente Home de lucide-react
 import { Home } from "lucide-react";
 // importamos el componente SidebarDesktop
 import { SidebarDesktop } from "./sidebar-desktop";
 
-// Definimos la función Sidebar que devuelve el componente SidebarDesktop
+// Definimos la función Sidebar que devuelve el componente SidebarDesktop       
 export function Sidebar() {
   return (
     <SidebarDesktop
