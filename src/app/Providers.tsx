a5265a2ddaee2c5bdffa30a103e3c6a6be7d24dc
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-26 22:29:30 -0400

    Actualizar providers de autenticación en el componente Providers.tsx

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,11 +1,8 @@
+"use client";
 // Importar los modulos de react necesarios para el componente de proveedor de sesion
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
 // Importar el componente de proveedor de sesion de la aplicacion de next-auth para la autenticacion de usuarios
-function Provider({ children} : { children: React.ReactNode }) {
-    return (
-        <SessionProvider>
-            {children}
-        </SessionProvider>
-    )
+function Provider({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
