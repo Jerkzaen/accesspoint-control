e1d877ccc4d4d1d7748fad9509795b1c94027459
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-26 22:01:34 -0400

    Actualizar providers de autenticación en la API de autenticación

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,5 +5,8 @@ import GoogleProvider from "next-auth/providers/google";
 
 //  exportamos la funcion de nextauth y le pasamos un objeto con los providers que vamos a usar
 NextAuth({
-  providers: [GoogleProvider({})],
+  providers: [GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  })],
 });
