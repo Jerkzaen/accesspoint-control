817d60bc34dad6e24edbcfee296f268236127511
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-26 22:29:50 -0400

    chore: Update Providers.tsx to export default Provider

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -6,3 +6,4 @@ import { SessionProvider } from "next-auth/react";
 function Provider({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
+export default Provider;
