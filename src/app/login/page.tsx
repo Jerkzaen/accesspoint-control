d7ba044b0cff5b6be137e2938b5b7c1af163ccf8
Jerkzaen <jersonarmijo4@gmail.com>
2024-05-28 22:42:30 -0400

    chore: Update npm dependencies
    ```

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+function LoginPage() {
+
+    
+    return (
+        <div>
+            <form>
+                <input type="email" placeholder="email"
+                />
+                <input type="password" placeholder="password"
+                />
+                <button>
+                    Login
+                </button>
+            </form>
+        </div>
+    );
+}
+export default LoginPage;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,18 @@
+function RegisterPage() {
+
+    
+    return (
+        <div>
+            <form>
+                <input type="email" placeholder="email"
+                />
+                <input type="password" placeholder="password"
+                />
+                <button>
+                    Register
+                </button>
+            </form>
+        </div>
+    );
+}
+export default RegisterPage;
