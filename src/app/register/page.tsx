1bf29f3a265f5aca1e824c71fea879db38a1e77d
Jerkzaen <jersonarmijo4@gmail.com>
2024-06-01 13:27:07 -0400

    chore: Update registration page with improved form layout and styling

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,18 +1,28 @@
 function RegisterPage() {
-
-    
-    return (
-        <div>
-            <form>
-                <input type="email" placeholder="email"
-                />
-                <input type="password" placeholder="password"
-                />
-                <button>
-                    Register
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="username"
+          name="fullname"
+          className="bg-zinc-800 px-4 py-2 rounded-lg block mb-2"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          className="bg-zinc-800 px-4 py-2 rounded-lg block mb-2"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          className="bg-zinc-800 px-4 py-2 rounded-lg block mb-2"
+        />
+        <button className="bg-black">Register</button>
+      </form>
+    </div>
+  );
 }
 export default RegisterPage;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from "./theme-toggle-button";
 // Define the Navbar component
 function Navbar() {
   return (
-    <div className="h-full px-3 py-4 flex justify-end my-2">
+    <div className="h-full px-3 py-4 flex justify-end my-2 border-2 border-r">
       <div className="flex gap-x-2 mb-1 mt-1 items-center">
         <ModeToggle />
       </div>
