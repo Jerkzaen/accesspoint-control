8727f06b809f09ff63a65a5ad50bbf69fa671218
Jerkzaen <jersonarmijo4@gmail.com>
2024-06-08 20:09:03 -0400

    chore: Update TicketCard and TicketForm components

--- src/components/TicketCard.tsx
+++ src/components/TicketCard.tsx
@@ -2,6 +2,7 @@
 // Importar React y el hook de estado de React para la aplicacion
 
 import { Badge } from "./ui/badge";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import {
   Table,
   TableBody,
@@ -33,81 +34,164 @@ interface Ticket {
 function TaskCard({ ticket }: { ticket: Ticket }) {
   // Renderiza el componente
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="">Numero de Caso </TableHead>
-          <TableHead className="">Empresa</TableHead>
-          <TableHead className="">Tecnico</TableHead>
-          <TableHead className="">Prioridad</TableHead>
-          <TableHead className="">Tipo</TableHead>
-          <TableHead className="">ID Notebook</TableHead>
-          <TableHead className="">Ubicacion</TableHead>
-          <TableHead className="">Contacto</TableHead>
-          <TableHead className="">Fecha de Creacion</TableHead>
-          <TableHead className="">Descripcion</TableHead>
-          <TableHead className="">Accion</TableHead>
-          <TableHead className="">Fecha de Solucion</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody key={ticket.nroCaso}>
-        <TableRow className="bg-accent">
-          <TableCell>
-            <div className="font-medium">{ticket.nroCaso}</div>
-          </TableCell>
-          <TableCell>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              {ticket.empresa}
-            </div>
-          </TableCell>
-          <TableCell>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              {ticket.tecnico}
-            </div>
-          </TableCell>
-          <TableCell>
-            <Badge>{ticket.prioridad}</Badge>
-          </TableCell>
-          <TableCell>
-            <Badge>{ticket.tipo}</Badge>
-          </TableCell>
-          <TableCell>
-            <Badge>{ticket.idNotebook}</Badge>
-          </TableCell>
-          <TableCell>
-            <Badge>{ticket.ubicacion}</Badge>
-          </TableCell>
-          <TableCell>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              {ticket.contacto}
-            </div>
-          </TableCell>
-          <TableCell>
-            <Badge>
-              {ticket.createdAt
-                ? new Date(ticket.createdAt).toLocaleDateString()
-                : ""}
-            </Badge>
-          </TableCell>
-          <TableCell>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              {ticket.descripcion}
-            </div>
-          </TableCell>
-          <TableCell>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              {ticket.accion}
-            </div>
-          </TableCell>
-          <TableCell>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              {ticket.fechaSolucion ? new Date(ticket.fechaSolucion).toLocaleDateString() : ""}
-            </div>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
-  );
+    <Card>
+      <CardHeader className="px-7">
+        <CardTitle>Orders</CardTitle>
+        <CardDescription>Recent orders from your store.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead className="hidden sm:table-cell">Type</TableHead>
+              <TableHead className="hidden sm:table-cell">Status</TableHead>
+              <TableHead className="hidden md:table-cell">Date</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow className="bg-accent">
+              <TableCell>
+                <div className="font-medium">Liam Johnson</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  liam@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Olivia Smith</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  olivia@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Refund</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="outline">
+                  Declined
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+              <TableCell className="text-right">$150.00</TableCell>
+            </TableRow>
+            <TableRow>
+                          <TableCell>
+                            <div className="font-medium">Liam Johnson</div>
+                            <div className="hidden text-sm text-muted-foreground md:inline">
+                              liam@example.com
+                            </div>
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            Sale
+                          </TableCell>
+                          <TableCell className="hidden sm:table-cell">
+                            <Badge className="text-xs" variant="secondary">
+                              Fulfilled
+                            </Badge>
+                          </TableCell>
+                          <TableCell className="hidden md:table-cell">
+                            2023-06-23
+                          </TableCell>
+                          <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow> 
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Noah Williams</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  noah@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">
+                Subscription
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-25</TableCell>
+              <TableCell className="text-right">$350.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Emma Brown</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  emma@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+              <TableCell className="text-right">$450.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Liam Johnson</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  liam@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Olivia Smith</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  olivia@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Refund</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="outline">
+                  Declined
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+              <TableCell className="text-right">$150.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Emma Brown</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  emma@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+              <TableCell className="text-right">$450.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  )
 }
+
+ 
 // Exporta el componente
 export default TaskCard;
