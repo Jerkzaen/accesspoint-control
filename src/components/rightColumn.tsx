f6f47f19f987dee9f415d2832ab4e53efaa3ae27
Jerson Armijo <jersonarmijo4@gmail.com>
2024-06-15 19:14:58 -0400

    feat: Agregar componente RightColumn al layout principal
    Se ha agregado el componente RightColumn al layout principal para mostrar una columna derecha en la página. Esto mejora la estructura del layout al proporcionar una sección adicional para contenido relevante en todas las páginas.

--- src/components/rightColumn.tsx
+++ src/components/rightColumn.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function rightCloumn() {
   return (
-    <div>rightCloumn</div>
+    <div className='md:mt-20 flex justify-centert md:w-[35%] bg-gray-400 min-h-[45%]'>
+      <p>Right Column</p>
+      <p>Right Column</p>
+    </div>
   )
 }
 
