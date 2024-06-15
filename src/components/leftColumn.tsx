d9b7ba8ad32b71b7821112de6a5649424414dfeb
Jerson Armijo <jersonarmijo4@gmail.com>
2024-06-15 19:21:30 -0400

    feat: Actualizar estilos de las columnas en el layout principal
    Se han actualizado los estilos de las columnas en el layout principal para mejorar la apariencia y la estructura de la página. Se han corregido las clases CSS en los componentes LeftColumn y RightColumn para alinear correctamente los elementos y ajustar los tamaños de las columnas. Estos cambios proporcionan una mejor experiencia visual y una distribución más equilibrada del contenido en todas las páginas.

--- src/components/leftColumn.tsx
+++ src/components/leftColumn.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const leftColumn = () => {
   return (
-    <div className=' mt-20 flex justify-between md:w-[65%] bg-gray-300 min-h-[45%]'>
+    <div className=' flex justify-between md:w-[65%] bg-gray-300 min-h-[45%]'>
         <p>Col-1 Start</p>
         <p>Col-2 End</p>
     </div>
--- src/components/rightColumn.tsx
+++ src/components/rightColumn.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function rightCloumn() {
   return (
-    <div className='md:mt-20 flex justify-centert md:w-[35%] bg-gray-400 min-h-[45%]'>
+    <div className='flex justify-between md:w-[35%] bg-gray-400 min-h-[45%]'>
       <p>Right Column</p>
       <p>Right Column</p>
     </div>
