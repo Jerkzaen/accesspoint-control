cf0fe4e15aa484505413086cb73fa8447d7cd1fb
Jerson Armijo <jersonarmijo4@gmail.com>
2024-06-15 19:00:58 -0400

    feat: Actualizar clase CSS para el elemento main en el componente RootLayout

--- src/components/leftColum.tsx
+++ src/components/leftColum.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const leftColum = () => {
   return (
-    <div>
+    <div className=' mt-20 flex justify-between md:w-[65%] bg-gray-300 min-h-[45%]'>
         <p>Col-1 Start</p>
         <p>Col-2 End</p>
     </div>
