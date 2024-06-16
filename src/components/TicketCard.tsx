44b685079cd1e68a972f4881402d3b00fffd0e94
Jerson Armijo <jersonarmijo4@gmail.com>
2024-06-16 11:30:14 -0400

    chore: Actualizar estilos de las columnas en el layout principal

--- src/components/TicketCard.tsx
+++ src/components/TicketCard.tsx
@@ -15,7 +15,21 @@ import {
   TableRow,
   TableHeader,
   TableHead,
+  TableFooter,
+  TableCaption,
 } from "./ui/table";
+import Ticket from "@/models/Ticket";
+import { connectDB } from "@/utils/mongoose";
+
+// Funcion para cargar los tickets de la base de datos
+async function loadTickets() {
+  //conexion a la base de datos
+  connectDB();
+  //buscar todos los tickets en la base de datos
+  const tickets = await Ticket.find();
+  //retornar los tickets
+  return tickets;
+}
 
 // Definir la  interfaz de ticket para el componente de tarjeta de ticket
 interface Ticket {
@@ -37,100 +51,40 @@ interface Ticket {
 
 
 // Definir el componente de la tarjeta de ticket  en el dashboard del usuario logeado en la aplicacion
-function TaskCard({ ticket }: { ticket: Ticket }) {
+async function TaskCard({ ticket }: { ticket: Ticket }) {
   let idPlano = JSON.parse(JSON.stringify(ticket._id));
+  const tickets = await loadTickets();
   // Renderiza el componente
   return (
-    <div className="flex flex-grow-0 flex-shrink-1 relative">
-      <Card key={ticket.nroCaso} className="sticky">
-        <CardHeader className="px-7">
-          <CardTitle>ID Caso</CardTitle>
-          <CardDescription>{idPlano}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Table className="flex flex-col justify-normal flex-grow-0 flex-shrink-1 sm:table-cell ">
-            <TableHeader>
-              <TableRow>
-                <TableHead className="hidden sm:table-cell">N° Caso</TableHead>
-                <TableHead className="hidden sm:table-cell">Empresa</TableHead>
-                <TableHead className="hidden sm:table-cell">
-                  Prioridad
-                </TableHead>
-                <TableHead className="hidden sm:table-cell">Tecnico</TableHead>
-                <TableHead className="hidden sm:table-cell">Tipo</TableHead>
-                <TableHead className="hidden sm:table-cell">Titulo</TableHead>
-                <TableHead className="hidden sm:table-cell">
-                  Id Notebook
-                </TableHead>
-                <TableHead className="hidden sm:table-cell">
-                  Ubicacion
-                </TableHead>
-                <TableHead className="hidden sm:table-cell">Contacto</TableHead>
-                <TableHead className="hidden sm:table-cell">
-                  Fecha Creacion
-                </TableHead>
-                <TableHead className="hidden sm:table-cell">
-                  Descripcion
-                </TableHead>
-                <TableHead className="hidden sm:table-cell">Accion</TableHead>
-                <TableHead className="hidden sm:table-cell ">
-                  Fecha Solucion
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow className="bg-accent">
-                <TableCell>
-                  <div id="nroCaso" className="font-medium">
-                    {ticket.nroCaso}
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <div id="empresa" className="font-medium">
-                    {ticket.empresa}
-                  </div>
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  <Badge>{ticket.prioridad}</Badge>
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.tecnico}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.tipo}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.titulo}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.idNotebook}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.ubicacion}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.contacto}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.createdAt
-                    ? new Date(ticket.createdAt).toLocaleDateString()
-                    : ""}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.descripcion}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.accion}
-                </TableCell>
-                <TableCell className="hidden sm:table-cell">
-                  {ticket.fechaSolucion
-                    ? new Date(ticket.fechaSolucion).toLocaleDateString()
-                    : ""}
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </CardContent>
+    <div  className="flex flex-grow-1 relative">
+      <Card >
+      <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow >
+          <TableHead>Nro Caso</TableHead>
+          <TableHead>Empresa</TableHead>
+          <TableHead>Prioridad</TableHead>
+          <TableHead>Tecnico</TableHead>
+          <TableHead>Descripcion</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {tickets.map((ticket) => (
+          <TableRow key={ticket.nroCaso} >
+            <TableCell >{ticket.nroCaso}</TableCell>
+            <TableCell >{ticket.empresa}</TableCell>
+            <TableCell >{ticket.prioridad}</TableCell>
+            <TableCell >{ticket.tecnico}</TableCell>
+            <TableCell >{ticket.descripcion}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+        </TableRow>
+      </TableFooter>
+    </Table>
       </Card>
     </div>
   );
--- src/components/leftColumn.tsx
+++ src/components/leftColumn.tsx
@@ -1,12 +1,91 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
-const leftColumn = () => {
-  return (
-    <div className=' flex justify-between md:w-[65%] bg-gray-300 min-h-[45%]'>
-        <p>Col-1 Start</p>
-        <p>Col-2 End</p>
-    </div>
-  )
+import { connectDB } from "@/utils/mongoose";
+import Ticket from "@/models/Ticket";
+import TaskCard from "./TicketCard";
+
+
+
+// Funcion para cargar los tickets de la base de datos
+async function loadTickets() {
+  //conexion a la base de datos
+  connectDB();
+  //buscar todos los tickets en la base de datos
+  const tickets = await Ticket.find();
+  //retornar los tickets
+  return tickets;
 }
 
-export default leftColumn
+ const  LeftColumn = async () => {
+  const tickets = await loadTickets();
+  return (
+    <div className=" flex md:w-[65%] bg-gray-300 min-h-[45%] relative ">
+      <Card className="flex w-full flex-col">
+        <CardHeader>
+          <CardTitle className="flex flex-row-3 justify-between">
+            <Card>
+                <CardHeader className="pb-2">
+                  <CardTitle>Crear Ticket</CardTitle>
+                  <CardDescription className="max-w-lg text-balance leading-relaxed ">
+                    Para crear un ticket de soporte, haz clic en el botón de
+                    abajo.
+                  </CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  <Link href="/tickets/new">
+                    <Button>Crear Nuevo Ticket</Button>
+                  </Link>
+                </CardFooter>
+              </Card>
+              <Card>
+                <CardHeader className="pb-2 items-center">
+                  <CardDescription>Total Tickets</CardDescription>
+                  <CardTitle className="text-4xl"></CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-xs text-muted-foreground text-center">
+                    Total a la fecha
+                  </div>
+                  <div className="text-xs text-muted-foreground text-center"></div>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="pb-2 items-center">
+                  <CardDescription>Tickets Activos</CardDescription>
+                  <CardTitle className="text-4xl">4</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-xs text-muted-foreground text-center">
+                    Sin resolver
+                  </div>
+                </CardContent>
+              </Card>
+
+              </CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent className="flex relative ">
+          
+          <TaskCard ticket={tickets[0]} />
+          
+          
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default LeftColumn;
