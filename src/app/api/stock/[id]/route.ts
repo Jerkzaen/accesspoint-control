5720f36dcf67b1c3b48228b92fb5884d92f796c7
Jerson Armijo <jersonarmijo4@gmail.com>
2024-07-27 20:50:35 -0400

    feat: Update GET request in stock route to retrieve product by ID

--- src/app/api/stock/[id]/route.ts
+++ src/app/api/stock/[id]/route.ts
@@ -1,13 +1,33 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
-export function GET() {
-  return NextResponse.json({ message: "Obteniendo un elemento api/stock" });
+//Interface para obtener el id del producto
+interface Params {
+  params: { id: string };
 }
 
-export function DELETE() {
+// Obtener un producto por id
+export async function GET(request: Request, { params }: Params) {
+  try {
+    const producto = await prisma.stock.findFirst({
+        where: { idProducto: Number(params.id) },
+      });
+      return NextResponse.json(producto);
+    
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+    
+  }
+}
+
+//Eliminar un producto por id
+export function DELETE(request: Request) {
   return NextResponse.json({ message: "Eliminando un elemento api/stock" });
 }
 
-export function PUT() {
+//Actualizar un producto por id
+export function PUT(request: Request) {
   return NextResponse.json({ message: "Actualizando un elemento api/stock" });
 }
