18ae79cb28ed8937c88040eb411b15c78d2fa3af
Jerson Armijo <jersonarmijo4@gmail.com>
2025-06-01 12:11:30 -0400

    Refactorizar el componente TicketsDashboardPage: mejorar la legibilidad del código y eliminar comentarios innecesarios.

--- src/app/tickets/dashboard/page.tsx
+++ src/app/tickets/dashboard/page.tsx
@@ -1,16 +1,14 @@
-import LeftColumnTickets from "@/components/LeftColumnTickets";
+    import LeftColumnTickets from "@/components/LeftColumnTickets";
 
-export default function TicketsDashboardPage() {
-  return (
-    // Este div debe permitir que LeftColumnTickets se expanda.
-    // 'h-full' para tomar la altura del <main> de layout.tsx.
-    // 'flex flex-col' para que LeftColumnTickets (que tiene h-full en su raíz) funcione.
-    // El padding general (p-4) se añade aquí para el contenido de la página.
-    <div className="flex flex-col h-full p-4">
-      <LeftColumnTickets />
-    </div>
-  );
-}
-
-      
-      
+    export default function TicketsDashboardPage() {
+      return (
+        // Este div debe permitir que LeftColumnTickets se expanda.
+        // 'h-full' para tomar la altura del <main> de layout.tsx.
+        // 'flex flex-col' para que LeftColumnTickets (que tiene h-full en su raíz) funcione.
+        // El padding general (p-4) se añade aquí para el contenido de la página.
+        <div className="flex flex-col h-full p-4">
+          <LeftColumnTickets />
+        </div>
+      );
+    }
+    
