7829cf81fd4e697c0421a82abc466c0e3631203b
Jerson Armijo <jersonarmijo4@gmail.com>
2025-06-01 11:25:49 -0400

    Eliminar referencia a SessionProvider en el componente ThemeProvider

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,12 @@
-"use client"
- 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
-// import { SessionProvider } from "next-auth/react"; // Comentado temporalmente
- 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  // return <SessionProvider><NextThemesProvider {...props}>{children}</NextThemesProvider></SessionProvider>; // Comentado temporalmente
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>; // Quitado SessionProvider
-}
+        "use client"
+        
+        import * as React from "react"
+        import { ThemeProvider as NextThemesProvider } from "next-themes"
+        import { type ThemeProviderProps } from "next-themes/dist/types"
+        // import { SessionProvider } from "next-auth/react"; // Eliminado
+        
+        export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+          // return <SessionProvider><NextThemesProvider {...props}>{children}</NextThemesProvider></SessionProvider>; // Eliminado
+          return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+        }
+        
