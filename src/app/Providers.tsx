68b4e1842ca5658aad89c9ba57ad304206f8d45b
Jerson Armijo <jersonarmijo4@gmail.com>
2025-06-01 11:25:24 -0400

    Eliminar referencia a SessionProvider en el componente Providers

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,9 +1,8 @@
-"use client";
-// Importar los modulos de react necesarios para el componente de proveedor de sesion
-// import { SessionProvider } from "next-auth/react"; // Comentado temporalmente
+        "use client";
+        // import { SessionProvider } from "next-auth/react"; // Eliminado
 
-// Importar el componente de proveedor de sesion de la aplicacion de next-auth para la autenticacion de usuarios
-export function Providers({ children }: { children: React.ReactNode }) {
-  // return <SessionProvider>{children}</SessionProvider>; // Comentado temporalmente
-  return <>{children}</>; // Devuelve solo children si SessionProvider se elimina
-}
+        export function Providers({ children }: { children: React.ReactNode }) {
+          // return <SessionProvider>{children}</SessionProvider>; // Eliminado
+          return <>{children}</>; // Devuelve solo children
+        }
+        
